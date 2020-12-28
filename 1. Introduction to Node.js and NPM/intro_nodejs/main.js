c594dab38c50a6428109c51967f720ef8033b119
Jeremiah Bassey Asuquo <31101804+jerryasuquo@users.noreply.github.com>
2020-12-28 14:48:44 +0100

    Adding initial files

---
+++ 1. Introduction to Node.js and NPM/intro_nodejs/main.js
@@ -0,0 +1,11 @@
+const helper = require("./helper");
+const moreHelpers = require("./moreHelpers");
+const evenMoreHelpers = require("./evenMoreHelpers");
+
+helper.sayHi();
+
+console.log(moreHelpers.firstName);
+moreHelpers.sayHello();
+moreHelpers.sayGoodbye();
+
+evenMoreHelpers();
